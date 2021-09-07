9d1601f87b62cd8ae612d46abf2303c6c1802574
hwkang93 <landman93@naver.com>
2021-09-07 09:46:27 +0900

    백준 단계별로 풀어보기 파트8 (기본 수학 1)

--- src/level8/Level8_3.java
+++ src/level8/Level8_3.java
@@ -1,26 +1,50 @@
 package level8;
 
+import java.util.Scanner;
+
+/**
+ *  1193. 분수찾기
+ *      무한히 큰 배열에 다음과 같이 분수들이 적혀 있다.
+ *          1/1 1/2 1/3 1/4 1/5 ...
+ *          2/1 2/2 2/3 2/4 ...
+ *          3/1 3/2 3/3 ...
+ *          4/1 4/2 ...
+ *          5/1 ...
+ *      이와 같이 나열된 분수들을 1/1 → 1/2 → 2/1 → 3/1 → 2/2 → … 과 같은 지그재그 순서로 차례대로 1번, 2번, 3번, 4번, 5번, … 분수라고 하자.
+ *      X가 주어졌을 때, X번째 분수를 구하는 프로그램을 작성하시오.
+ */
 public class Level8_3 {
     public static void main(String[] args) {
-        /*
-            1 1 2 3 2 1 1 2 3 4 5 4 3 2 1 1 2 3 4 5 6 7 6 5 4 3 2 1
-            - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-            1 2 1 1 2 3 4 3 2 1 1 2 3 4 5 6 4 3 2 1 1 2 3 4 5 6 7 8
-         */
-        /*
-         n
-         0 => 1                             1
-         1 => 1 2 3 2 1                     5
-         2 => 1 2 3 4 5 4 3 2 1             9
-         3 => 1 2 3 4 5 6 7 6 5 4 3 2 1     13
-        */
-        int x = 11;
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
 
-        int row = 1;
-        int sum = 0;
-        while(true) {
+        int num = 1;        //줄 바꾸는 구분자. 첫째줄부터 시작해서 1로 초기화함
+        int count = 0;      //면 번째 까지 왔는지
+        int top = 0;        //분자값
+        int bottom = 0;     //분모값
 
+        while (x != count) {
+            for(int i = 0; i < num; i++) {
+                //n이 짝수이면 분자는 순행 분모는 역행
+                if(num % 2 == 0) {
+                    top = i + 1;
+                    bottom = num - i;
+                }
+                //n이 홀수이면 분자는 역행 분모는 순행
+                else {
+                    top = num - i;
+                    bottom = i + 1;
+                }
+
+                count ++;
+                if(count == x) {
+                    break;
+                }
+            }
+
+            num++;
         }
 
+        System.out.println(top + "/" + bottom);
     }
 }
