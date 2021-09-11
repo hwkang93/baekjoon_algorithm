832561caa952426827859c87f9bae0509056b64c
hwkang <landman93@naver.com>
2021-09-11 22:49:25 +0900

    백준 단계별로 풀어보기 파트10 (재귀)

--- src/level10/Level10_1.java
+++ src/level10/Level10_1.java
@@ -2,6 +2,10 @@ package level10;
 
 import java.util.Scanner;
 
+/**
+ *  10872. 팩토리얼
+ *      0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+ */
 public class Level10_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
