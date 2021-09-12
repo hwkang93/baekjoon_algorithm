96291ce3475c20ad27c28d61eae180932743f8b0
hwkang <landman93@naver.com>
2021-09-13 00:02:48 +0900

    백준 단계별로 풀어보기 파트10 (재귀)

---
+++ src/level10/Level10_4.java
@@ -0,0 +1,16 @@
+package level10;
+
+public class Level10_4 {
+    public static void main(String[] args) {
+        int n = 3;
+    }
+
+    public static void move(int a, int b, int c, int count) {
+        //옮길 게 없는 경우
+        if(a == 0 && b == 0) {
+
+        }
+
+
+    }
+}
---
+++ src/level11/Level11_1.java
@@ -0,0 +1,7 @@
+package level11;
+
+public class Level11_1 {
+    public static void main(String[] args) {
+
+    }
+}
