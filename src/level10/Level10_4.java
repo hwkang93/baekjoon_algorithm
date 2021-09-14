0ecccafdc566e04f7f64d3497f32e61b57830525
hwkang <landman93@naver.com>
2021-09-14 15:06:09 +0900

    백준 단계별로 풀어보기 파트10 (재귀)

--- src/level10/Level10_4.java
+++ src/level10/Level10_4.java
@@ -1,16 +1,45 @@
 package level10;
 
+import java.util.Scanner;
+
+/**
+ *  11729. 하노이 탑 이동 순서
+ *      세 개의 장대가 있고 첫 번째 장대에는 반경이 서로 다른 n개의 원판이 쌓여 있다. 각 원판은 반경이 큰 순서대로 쌓여있다.
+ *      이제 수도승들이 다음 규칙에 따라 첫 번째 장대에서 세 번째 장대로 옮기려 한다.
+ *          1. 한 번에 한 개의 원판만을 다른 탑으로 옮길 수 있다.
+ *          2. 쌓아 놓은 원판은 항상 위의 것이 아래의 것보다 작아야 한다.
+ *      이 작업을 수행하는데 필요한 이동 순서를 출력하는 프로그램을 작성하라. 단, 이동 횟수는 최소가 되어야 한다.
+ *
+ *      첫째 줄에 첫 번째 장대에 쌓인 원판의 개수 N (1 ≤ N ≤ 20)이 주어진다.
+ *
+ *      첫째 줄에 옮긴 횟수 K를 출력한다.
+ *      두 번째 줄부터 수행 과정을 출력한다. 두 번째 줄부터 K개의 줄에 걸쳐 두 정수 A B를 빈칸을 사이에 두고 출력하는데,
+ *      이는 A번째 탑의 가장 위에 있는 원판을 B번째 탑의 가장 위로 옮긴다는 뜻이다.
+ */
 public class Level10_4 {
+
+    public static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) {
-        int n = 3;
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        sb.append((int) (Math.pow(2, n) -1)).append('\n');
+
+        hanoi(n, 1, 2, 3);
+        System.out.println(sb);
     }
 
-    public static void move(int a, int b, int c, int count) {
-        //옮길 게 없는 경우
-        if(a == 0 && b == 0) {
-
+    public static void hanoi(int n, int start, int mid, int to) {
+        if(n == 1) {
+            sb.append(start + " " + to + "\n");
+            return;
         }
 
+        hanoi(n-1, start, to, mid);
 
+        sb.append(start + " " + to + "\n");
+
+        hanoi(n-1, mid, start, to);
     }
 }
---
+++ src/level10/README.md
@@ -0,0 +1,8 @@
+# 하노이 탑 이동 순서
+[백준 알고리즘 11729. 하노이 탑 이동 순서 문제 바로가기](https://www.acmicpc.net/problem/11729)
+
+
+
+참고 블로그
+
+[Stranger's LAB 블로그 바로가기](https://st-lab.tistory.com/96)
