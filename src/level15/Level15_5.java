ff5cf0fb7851ccc4bd896d25091ad321d1682cb0
hwkang <landman93@naver.com>
2021-10-10 22:59:37 +0900

    백준 단계별로 풀어보기 파트15 (동적 계획법 1)

---
+++ src/level15/Level15_5.java
@@ -0,0 +1,30 @@
+package level15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Level15_5 {
+
+    public static final int RED = 0;
+    public static final int GREEN = 1;
+    public static final int BLUE = 2;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        StringTokenizer stringTokenizer;
+
+        int n = Integer.parseInt(bufferedReader.readLine());
+
+        for(int i=0; i<n; i++) {
+
+        }
+    }
+
+    public static void get(int index, int color) {
+
+    }
+}
